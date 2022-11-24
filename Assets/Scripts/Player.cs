aaf54d632b0826979f615cc9c01d293521d48a62
Vladislav Bystritskiy <mrevlring@gmail.com>
2022-11-24 15:25:58 +0300

    add back speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,28 +8,35 @@ public class Player : MonoBehaviour
     private float _speed = 5;
 
     [SerializeField]
+    private float _backSpeed = 3;
+
+    [SerializeField]
     private float _rotateSpeed = 90;
 
     private void Update()
     {
-        float v = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        float direction = Input.GetAxis("Vertical");
+        float v;
         float h;
 
-        if (v > 0)
+        if (direction > 0)
         {
+            v = direction * _speed * Time.deltaTime;
             h = Input.GetAxis("Horizontal") * _rotateSpeed * Time.deltaTime * -1;
         }
-        else if (v < 0)
+        else if (direction < 0)
         {
+            v = direction * _backSpeed * Time.deltaTime;
             h = Input.GetAxis("Horizontal") * _rotateSpeed * Time.deltaTime * 1;
         }
         else
         {
+            v = 0;
             h = 0;
         }
+
         Vector3 vertical = transform.TransformDirection(Vector3.up);
         transform.localPosition += vertical * v;
-
         transform.Rotate(new Vector3(0, 0, h));
     }
 }
